2710fe327dcea246019c8acca8665f99a2a7a57f
Ariz <arinopc22@gmail.com>
2025-09-22 19:26:44 +0530

    Hide Navbar in login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './App.css'
@@ -9,10 +9,12 @@ import LoginPage from './components/Pages/LoginPage'
 import SignupPage from './components/Pages/SignupPage'
 
 function App() {
+  const location = useLocation();
+  const hideHeader = location.pathname.startsWith('/login') || location.pathname.startsWith('/signup');
 
   return (
     <div id="main-page" className="page-container" style={{ width: '100%' }}>
-      <Header />
+      {!hideHeader && <Header />}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
